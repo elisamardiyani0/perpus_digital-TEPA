f631f8df093f0e6059e4a0ce8ce16609f911cf5d
elisamardiyani0 <141213160+elisamardiyani0@users.noreply.github.com>
2024-02-26 16:01:17 +0700

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container-fluid">
+        <div class="row my-5">
+            <div class="col-lg-6">
+                <nuxt-link to="/pengunjung/tambah">
+                    <div class="card bg-pengunjung rounded-5">
+                        <div class="card-body">
+                            <h2>Pengunjung</h2>
+                        </div>
+                    </div>
+                </nuxt-link>
+            </div>
+
+            <div class="col-lg-6">
+                <nuxt-link to="/buku">
+                    <div class="card bg-buku rounded-5">
+                        <div class="card-body">
+                            <h2>Cari Buku</h2>
+                        </div>
+                    </div>
+                </nuxt-link>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="card kunjungan rounded-5 mt-5">
+                    <div class="card-body">
+                        <h2>Jumlah Pengunjung</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+                <div class="card buku rounded-5 mt-5">
+                    <div class="card-body">
+                        <h2>Jumlah Buku</h2>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.card {
+    height: 250px;
+    box-shadow: 1px 1px 10px #424242 ;
+}
+.card.bg-pengunjung{
+    background-image: url('../assets/img/bg-kunjungan.jpeg');
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+    color: black;
+}
+.card.bg-buku{
+    background: url('../assets/img/bg-home.jpg') no-repeat center center;
+    background-size: cover;
+    color: black;
+}
+.card.kunjungan{
+    background-color:#EEE063 ;
+}
+.card.buku{
+    background-color: #AEEEC7;
+}
+</style>
