9b2e62fe6596d13afdfb526bf0419fe414336ce1
elisamardiyani0 <141213160+elisamardiyani0@users.noreply.github.com>
2024-05-14 13:30:55 +0700

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -59,7 +59,7 @@
       box-shadow: 1px 1px 10px #424242;
     }
     .card.bg-pengunjung {
-      background-image: url("../assets/img/bg-kunjungan.webp");
+      background-image: url("../assets/img/bg-kunjungan1.webp");
       background-repeat: no-repeat;
       background-position: center center;
       background-size: cover;
@@ -67,7 +67,7 @@
       opacity: 50%;
     }
     .card.bg-buku {
-      background: url("../assets/img/bg-home.webp") no-repeat center center;
+      background: url("../assets/img/bg-home1.webp") no-repeat center center;
       background-size: cover;
       color: #180101;
       opacity: 50%;
